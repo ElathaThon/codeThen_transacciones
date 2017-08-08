ebcbdd16c1d8dc0a976d2a9d40ee5338c9863bf7
Elatha <bernat.90@gmail.com>
2017-08-08 18:06:42 +0200

    Basic maven to start the server

---
+++ src/main/java/com/topoinventari/transacciones/TransaccionesApplication.java
@@ -0,0 +1,30 @@
+package com.topoinventari.transacciones;
+
+
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class TransaccionesApplication extends Application<TransaccionesConfiguration> {
+//Alt + intro per desplegar ho de sugerencias de errores
+
+    public static void main(String[] args) throws Exception {
+        new TransaccionesApplication().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "books!!!! :D";
+    }
+
+    @Override
+    public void initialize(Bootstrap<TransaccionesConfiguration> bootstrap) {
+        // nothing to do yet
+    }
+
+    @Override
+    public void run(TransaccionesConfiguration configuration, Environment environment) {
+
+
+    }
+}
---
+++ src/main/java/com/topoinventari/transacciones/TransaccionesConfiguration.java
@@ -0,0 +1,9 @@
+package com.topoinventari.transacciones;
+
+import io.dropwizard.Configuration;
+
+public class TransaccionesConfiguration extends Configuration {
+
+    //Nothing for now, es la configuracio de dropwidarz, ens la demana....
+
+}
