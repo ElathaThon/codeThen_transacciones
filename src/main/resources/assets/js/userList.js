cff34b2182f8c5dc3fa8c1b089dc0328a0da5114
Elatha <bernat.90@gmail.com>
2017-08-24 16:20:04 +0200

    JavaScript only for userList

--- src/main/resources/assets/js/userList.js
+++ src/main/resources/assets/js/userList.js
@@ -1,7 +1,13 @@
 
+/**
+ * setup all elements that we need
+ * */
 setupSearchUserButton();
 
-/** Setup the button in userList to perform the search of the user that is introduced in the input value */
+
+/**
+ * Setup the button in userList to perform the search of the user that is introduced in the input value
+ * */
 function setupSearchUserButton() {
 
     const div = $(".user.search");
@@ -12,16 +18,16 @@ function setupSearchUserButton() {
         console.log("Search for: " + input);
 
         if (input != null) {
-            const getUrl = window.location;
-            const baseUrl = getUrl .protocol + "//" + getUrl.host + "/" + getUrl.pathname.split('/')[1];
-            window.open(baseUrl+"?search="+input , "_self");
+            window.open(location.href = '/users?search=' +input , "_self");
             //setupFindAllUserButton();
         }
     });
 
 }
 
-/** Setup the button in userList to show the list of all the users */
+/**
+ * Setup the button in userList to show the list of all the users
+ * */
 function setupFindAllUserButton() {
     const div = $(".user.search");
     const button = div.find("button");
